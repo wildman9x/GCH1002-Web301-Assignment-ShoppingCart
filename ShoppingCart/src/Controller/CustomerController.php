6ef6b73f4823d1723338971bb3a62fb03902e2b1
Nghiadao1 <nghiadtgbh200708@fpt.edu.vn>
2022-06-27 16:36:32 +0700

    update

--- ShoppingCart/src/Controller/CustomerController.php
+++ ShoppingCart/src/Controller/CustomerController.php
@@ -3,12 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Customer;
+use App\Form\CustomerType;
 use App\Repository\ProductRepository;
 use App\Repository\CustomerRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 
 
 #[Route('/customer')]
