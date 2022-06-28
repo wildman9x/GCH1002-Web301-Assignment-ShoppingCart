55097de8e8c30e3f5335f9e339184f3bcfd1f62e
wildman9x <wildmanyt@gmail.com>
2022-06-28 18:16:31 +0700

    fix create user

--- ShoppingCart/src/Controller/HomeController.php
+++ ShoppingCart/src/Controller/HomeController.php
@@ -2,17 +2,34 @@
 
 namespace App\Controller;
 
+use App\Repository\ImageRepository;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    #[Route('/', name: 'home')]
+    // Get all products and picture from database and display them in home page
+    // Use repository to get all products from database
+    // get one random image for welcome message, 3 random images for banner
+    public function index(ImageRepository $imageRepository, ProductRepository $productRepository)
     {
+        $image = $imageRepository->findAll();
+        $product = $productRepository->findAll();
+        // get a random image from $image
+        $randomImage = $imageRepository->findBy([], ['imageID' => 'ASC'], 1, 0);
+        // get 3 random images from $image
+        $randomImages = $imageRepository->findBy([], ['imageID' => 'ASC'], 3, 0);
+        
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'images' => $image,
+            'products' => $product,
+            'randomImage' => $randomImage,
+            'randomImages' => $randomImages
+            
         ]);
     }
 }
